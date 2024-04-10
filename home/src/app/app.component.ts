1e7041e7e1394bf2036b19176916ee92070c0a6f
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-04-10 07:11:02 +0000

    commit2

---
+++ home/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ListComponent } from './list/list.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, NavigationComponent, ListComponent, IntroductionComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'home';
+}
